87e1b1bac00e325cb7effde6c3fd9f3b39d09435
ufo <1923562793@qq.com>
2021-07-09 17:00:49 +0800

    米珂 第一次提交

---
+++ src/main/java/com/dancy/maintain/dao/check/DailyItemDao.java
@@ -0,0 +1,8 @@
+package com.dancy.maintain.dao.check;
+
+import com.dancy.maintain.pojo.check.RegularCheck;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface DailyItemDao extends JpaRepository<RegularCheck, Long>, JpaSpecificationExecutor<RegularCheck> {
+}
